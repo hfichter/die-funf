2da2ddc00dca7485c287d606b4e13eb47be7e7ef
Hugo Fichter <hfichter@users.noreply.github.com>
2024-01-19 00:31:04 +0100

    update layout

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,16 +1,10 @@
 <script>
 	import { page } from '$app/stores';
 	import { base } from '$app/paths';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
+	<div></div>
 
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -29,36 +23,13 @@
 		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+	<div></div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
 	}
 
 	nav {
