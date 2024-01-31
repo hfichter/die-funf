830268e78fd48f4431226fe15982c86c05882cee
Hugo Fichter <hfichter@users.noreply.github.com>
2024-02-01 00:38:38 +0100

    prerender players pages

--- src/routes/players/[id]/+page.svelte
+++ src/routes/players/[id]/+page.svelte
@@ -2,26 +2,14 @@
 	import { PB_URL } from '$lib/config.js';
 	import { converter } from '$lib/globals.js';
 	import PocketBase, { type RecordModel } from 'pocketbase';
-	import { onMount } from 'svelte';
 
 	export let data;
 
 	const pb = new PocketBase(PB_URL);
-	let player: RecordModel | null = null;
-
-	async function fetchPlayer() {
-		player = await pb.collection('players').getOne(data.id);
-	}
-	onMount(() => {
-		fetchPlayer();
-	});
+	let player: RecordModel = data.player;
 </script>
 
 <div class="text-column">
-	{#if player}
-		<h1 class="text-3xl mb-3">{player.name}</h1>
-		<p>{@html converter.makeHtml(player.data.description)}</p>
-	{:else}
-		<p>Chargement du funf...</p>
-	{/if}
+	<h1 class="text-3xl mb-3">{player.name}</h1>
+	<p>{@html converter.makeHtml(player.data.description)}</p>
 </div>
--- src/routes/players/[id]/+page.ts
+++ src/routes/players/[id]/+page.ts
@@ -1,3 +1,12 @@
-export function load({ params }) {
-	return params;
+import { PB_URL } from '$lib/config.js';
+import PocketBase, { type RecordModel } from 'pocketbase';
+
+
+export const prerender = true;
+
+export async function load(data) {
+
+	const pb = new PocketBase(PB_URL);
+	let player: RecordModel = await pb.collection('players').getOne(data.params.id);
+	return { player };
 }
